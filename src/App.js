edffcbee7dfefb6d8ac22d47b0e7244a4c85b6fc
SU KYUNG IM <99783474+oiosu@users.noreply.github.com>
2023-12-22 15:53:52 +0900

    feat: 장바구니 페이지 routing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from 'react';
 import MainPage from './Components/MainPage/MainPage';
 import Nav from './Components/MainPage/Nav';
 import Footer from './Components/MainPage/Footer';
-
+import Empty from "./Components/Basket/Empty";
 
 function App() {
   return (
-    <div className="App">
-  
-      <Nav/>
-      <MainPage/>
-      <Footer/>
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path='/empty' element={<Empty />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
