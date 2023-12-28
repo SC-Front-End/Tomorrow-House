4ab95e77e746f40282e965856092cd3838eee773
SU KYUNG IM <99783474+oiosu@users.noreply.github.com>
2023-12-28 12:11:01 +0900

    feat: main page 페이지 모두 완성

--- src/Components/MainPage/MainPage.jsx
+++ src/Components/MainPage/MainPage.jsx
@@ -3,7 +3,9 @@ import Banner from './Banner';
 import CategoryList from './CategoryList';
 import BestSale from './BestSale';
 import Ranking from './Ranking';
-
+import GiftItem from './GiftItem';
+import AdSection from './AdSection';
+import Use from './Use';
 
 
 const MainPage = () => {
@@ -13,6 +15,9 @@ const MainPage = () => {
             <CategoryList />
             <BestSale />
             <Ranking />
+            <GiftItem />
+            <AdSection />
+            <Use />
         </>
     );
 };
