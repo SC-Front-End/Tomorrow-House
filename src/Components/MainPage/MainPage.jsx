d0c6d47384d38755041d15a3ee9a8942f5a83418
SU KYUNG IM <99783474+oiosu@users.noreply.github.com>
2023-12-31 16:16:05 +0900

    feat: ranking best 다시 완성

--- src/Components/MainPage/MainPage.jsx
+++ src/Components/MainPage/MainPage.jsx
@@ -1,25 +1,24 @@
-import React from 'react';
-import Banner from './Banner';
-import CategoryList from './CategoryList';
-import BestSale from './BestSale';
-import Ranking from './Ranking';
-import GiftItem from './GiftItem';
-import AdSection from './AdSection';
-import Use from './Use';
-
+import React from "react";
+import Banner from "./Banner";
+import CategoryList from "./CategoryList";
+import BestSale from "./BestSale";
+import GiftItem from "./GiftItem";
+import AdSection from "./AdSection";
+import Use from "./Use";
+import RankingBest from "./RankingBest";
 
 const MainPage = () => {
-    return (
-        <>
-            <Banner />
-            <CategoryList />
-            <BestSale />
-            <Ranking />
-            <GiftItem />
-            <AdSection />
-            <Use />
-        </>
-    );
+  return (
+    <>
+      <Banner />
+      <CategoryList />
+      <BestSale />
+      <RankingBest />
+      <GiftItem />
+      <AdSection />
+      <Use />
+    </>
+  );
 };
 
 export default MainPage;
