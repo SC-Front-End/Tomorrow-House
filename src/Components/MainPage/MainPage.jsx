aa1f357e30b48a636870acd45eed1959ed118593
SU KYUNG IM <99783474+oiosu@users.noreply.github.com>
2024-01-02 14:06:23 +0900

    fix: main page 오류 수정

--- src/Components/MainPage/MainPage.jsx
+++ src/Components/MainPage/MainPage.jsx
@@ -1,24 +1,3 @@
-import React from 'react';
-import Banner from './Banner';
-import CategoryList from './CategoryList';
-import BestSale from './BestSale';
-import Ranking from './Ranking';
-import GiftItem from './GiftItem';
-import Use from './Use';
-
-
-const MainPage = () => {
-    return (
-        <>
-            <Banner />
-            <CategoryList />
-            <BestSale />
-            <Ranking />
-            <GiftItem />
-            <Use />
-        </>
-    );
-
 import React from "react";
 import Banner from "./Banner";
 import CategoryList from "./CategoryList";
@@ -28,16 +7,16 @@ import Use from "./Use";
 import RankingBest from "./RankingBest";
 
 const MainPage = () => {
-  return (
-    <>
-      <Banner />
-      <CategoryList />
-      <BestSale />
-      <RankingBest />
-      <GiftItem />
-      <Use />
-    </>
-  );
+    return (
+        <>
+            <Banner />
+            <CategoryList />
+            <BestSale />
+            <RankingBest />
+            <GiftItem />
+            <Use />
+        </>
+    );
 
 };
 
