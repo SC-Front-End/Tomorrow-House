7f66638e0469da9d842310fbdf1d2d3c8e0028e9
CASSIE1107 <dudnfsla1@naver.com>
2024-01-03 14:36:49 +0900

    feat: 결제완료페이지 버튼 클릭 후 주문상세페이지로 라우팅 설정

--- src/Components/Payment/PaymentDetailGuide.jsx
+++ src/Components/Payment/PaymentDetailGuide.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import styled from 'styled-components';
+import { useNavigate } from 'react-router';
 
 const PaymentDetailGuide = () => {
+
+    const navigate = useNavigate();
+
+    const handleButtonClick = () => {
+        // '/cart' 경로로 이동
+        navigate('/cart');
+    };
+
     return (
         <>
             <PaymentDetailGuideWrapper>
                 <PaymentIsFinishedImg src="https://www.codemshop.com/manual/wp-content/uploads/2022/11/%E1%84%80%E1%85%A7%E1%86%AF%E1%84%8C%E1%85%A6%E1%84%8B%E1%85%AA%E1%86%AB%E1%84%85%E1%85%AD.png" />
-                <PaymentDetailGuideButton>주문현황 보기</PaymentDetailGuideButton>
+                <PaymentDetailGuideButton  onClick={handleButtonClick}>주문현황 보기</PaymentDetailGuideButton>
             </PaymentDetailGuideWrapper>
         </>
     );
